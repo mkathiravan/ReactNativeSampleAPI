a7ea783721969a2e71ac92c8ff8242b602f406ea
Kathiravan <mkathiravanmca@gmail.com>
2021-06-15 17:43:29 +0530

    Update the App json file

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      dataSource: null
+    }
+  }
+
+  componentDidMount () {
+    this.apiCall();
+  }
+
+  async apiCall()
+  {
+    let resp = await fetch('https://facebook.github.io/react-native/movies.json')
+    let respJson = await resp.json()
+    console.log(respJson)
+    this.setState({
+      data: respJson.movies
+    })
+  }
+
+  render(){
+
+    return (
+      <View>
+        <Text style = {{fontSize : 40, marginTop: 30}}>API call Demo</Text>
+        <FlatList
+        data = {this.state.data}
+        renderItem={({item})=>
+         <Text style = {{fontSize: 40, backgroundColor:'skyblue',margin: 15}}>{item.title},{item.releaseYear}</Text>}
+        />
+      </View>
+    );
+
+  }
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
